2bcf00d2e967a04e243356a8f89496b6879a167b
shashi-we <shashi.we@gmail.com>
2025-04-27 00:38:23 +0530

    add sign up form - reuse sign in form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import { useState } from 'react'
 import Header from './Header';
 
 const Login = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true);
+
+  const toggleSignInForm = () => {
+    setIsSignInForm(!isSignInForm);
+  }
+
   return (
     <div>
       <Header />
@@ -12,10 +18,12 @@ const Login = () => {
         />
       </div>
       <form className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
-        <h1 className="font-bold text-3xl py-4">Sign In</h1>
+        <h1 className="font-bold text-3xl py-4">{ isSignInForm ? "Sign In" : "Sign Up"}</h1>
+        { !isSignInForm && <input type="text" placeholder="Full Name" className="p-2 my-4 w-full bg-gray-700 rounded-sm" />}
         <input type="text" placeholder="Email Address" className="p-2 my-4 w-full bg-gray-700 rounded-sm" />
         <input type="password" placeholder="Password" className="p-2 my-4 w-full bg-gray-700 rounded-sm" />
-        <button className="p-4 my-6 bg-red-700 w-full rounded-lg">Sign In</button>
+        <button className="p-4 my-6 bg-red-700 w-full rounded-lg">{ isSignInForm ? "Sign In" : "Sign Up"}</button>
+        <p className="py-4 cursor-pointer" onClick={toggleSignInForm }>{ isSignInForm ? "New to Netflix? Sign up now." : "Already registered? Sign in now."}</p>
       </form>
     </div>
   )
